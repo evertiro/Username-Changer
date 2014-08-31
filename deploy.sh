2ad45d63c7c93215ab73e6da9e63d6317d9d05a9
Ghost1227 <dgriffiths@section214.com>
2014-08-31 18:14:12 -0500

    Update user in deployment script

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG" # Remote SVN repo on wordpress.org, with no trailing slash
-SVNUSER="ghost1227" # your svn username
+SVNUSER="section214" # your svn username
 
 
 # Let's begin...
